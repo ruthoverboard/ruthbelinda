5423274e9dbc74f5aba05ff9cc5bf1879ffe2281
ruthceliceo94@gmail.com <ruthceliceo94@gmail.com>
2016-10-02 18:46:02 -0700

    Tarea 5

--- app/scripts/controllers/tarea05.js
+++ app/scripts/controllers/tarea05.js
@@ -8,10 +8,24 @@
  * Controller of the ruthbelindaApp
  */
 angular.module('ruthbelindaApp')
-  .controller('Tarea05Ctrl', function () {
+  .controller('Tarea05Ctrl', function ($scope, $http) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $scope.getWeather = function(){
+      var info = {Cities: $scope.cities.split(",")};
+      console.log(info);
+      var url = "https://i545hvxm8e.execute-api.us-east-1.amazonaws.com/dev/weatherMultipleRuth";
+
+      $http.put(url, info)
+        .success(function(info){
+        $scope.cityWeather = info;
+      })
+        .error(function(error){
+          alert(error);
+        })
+    };
+
   });
